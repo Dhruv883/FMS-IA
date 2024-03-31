f49ee3154d8efc837c96944fd86caa76b9fa6845
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-03-31 14:51:01 +0530

    Movie Detail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Test from "./pages/Test";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
+import { MovieDetail } from "./pages/MovieDetail";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/test" element={<Test />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/MovieDetail.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MovieDetail = () => {
+  return (
+    <div className="overflow-hidden h-screen">
+      <img src="/movieDetail.jpg" alt="" className="w-full relative z-10" />
+    </div>
+  );
+};
