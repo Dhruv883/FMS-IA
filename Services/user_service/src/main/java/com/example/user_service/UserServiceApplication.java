8abf9eecc4661bcaf9ad8204d669d6b4efba70bd
SuyeshJadhav <suyesh.j@somaiya.edu>
2024-04-14 15:39:47 +0530

    new

--- Services/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ Services/user_service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -2,12 +2,10 @@ package com.example.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
